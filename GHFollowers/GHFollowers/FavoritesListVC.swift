fb895a339ef558976a701c685bd94eb2a70304aa
manishparihar948 <manish.parihar948@gmail.com>
2024-01-11 15:51:49 +0100

    Added TabbarController and app icon

---
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Manish Parihar on 11.01.24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Manish Parihar on 11.01.24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
