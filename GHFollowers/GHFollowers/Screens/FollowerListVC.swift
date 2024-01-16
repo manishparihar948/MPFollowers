b3ed397c7f95dd4cc39c2b5e03be4cc79f97c1c9
manishparihar948 <manish.parihar948@gmail.com>
2024-01-16 13:09:16 +0100

    Refactor error messages

--- GHFollowers/GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListVC.swift
@@ -13,18 +13,17 @@ class FollowerListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { (followers,errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainTread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainTread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
     
